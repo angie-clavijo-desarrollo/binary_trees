4930bc15063f14af509c4124d0ec8c189e5a5ff0
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-07-21 18:58:34 -0500

    validate if tree is full or not

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -1,11 +1,20 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_full -
+ * binary_tree_is_full - validate if tree is full or not
  *
- * @tree:
- * Return: size_t
+ * @tree: parameter of node parent and child
+ * Return: 0 is not full or 1 is fully
  */
 int binary_tree_is_full(const binary_tree_t *tree)
 {
-	
+	if (tree == NULL)
+		return (0);
+
+	if (tree->left == NULL && tree->right == NULL)
+		return (1);
+
+	if ((tree->left) && (tree->right))
+		return (binary_tree_is_full(tree->left) && binary_tree_is_full(tree->right));
+
+	return (0);
 }
