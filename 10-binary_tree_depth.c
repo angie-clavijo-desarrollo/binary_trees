841f6eae06c0b20526707ede9db0f383055c3ee2
Sergioarg <sergio_ramos53@yahoo.com>
2021-07-20 23:12:52 -0500

    :heavy_plus_sign: Add function that get the depth of binary tree

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -1,11 +1,14 @@
 #include "binary_trees.h"
 /**
- * binary_tree_depth -
+ * binary_tree_depth - measures the depth of a node in a binary tree
  *
- * @tree:
- * Return: size_t
+ * @tree: pointer to the root node of the tree to measure the height.
+ * Return: max depth of the binary tree
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
+	if (tree == NULL || tree->parent == NULL)
+		return (0);
 
+	return (binary_tree_depth(tree->parent) + 1);
 }
--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -3,7 +3,7 @@
  * binary_tree_height - measures the height of a binary tree
  *
  * @tree: pointer to the root node of the tree to measure the height.
- * Return: size_t
+ * Return: max height of the binary tree
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
