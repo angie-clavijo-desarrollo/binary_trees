1b5aa901a7215aec71b6a263e90e02a97d9e488e
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-07-21 20:06:18 -0500

    tree binary is perfect or not

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -12,8 +12,32 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 		return (0);
 
 	if (tree->left == NULL && tree->right == NULL)
-		return (0);
+		return (1);
 
-	return (binary_tree_is_perfect(tree->left)
-		&& binary_tree_is_perfect(tree->right));
+	if (binary_tree_height(tree->right) == binary_tree_height(tree->right))
+		return (binary_tree_is_perfect(tree->left)
+			&& binary_tree_is_perfect(tree->right));
+	return(0);
+}
+
+size_t binary_tree_height (const binary_tree_t *tree)
+{
+	size_t c_left = 0;
+	size_t c_right = 0;
+
+	if (tree != NULL)
+	{
+		if (tree->left)
+			c_left = 1 + binary_tree_height(tree->left);
+
+		if (tree->right)
+			c_right = 1 + binary_tree_height(tree->right);
+
+		if (c_left > c_right)
+			return (c_left);
+
+		if (c_right > c_left)
+			return (c_right);
+	}
+	return (0);
 }
