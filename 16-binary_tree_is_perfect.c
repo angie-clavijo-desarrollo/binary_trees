c76136d17400756bd1af0e6228b402feb8fa476d
angie-clavijo-desarrollo <angiedesarrollo@gmail.com>
2021-07-21 19:24:58 -0500

    tree binary is perfect or not

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -1,11 +1,19 @@
 #include "binary_trees.h"
 /**
- * binary_tree_is_perfect -
+ * binary_tree_is_perfect - Function that validate if
+ * given tree is a preferct or not
  *
- * @tree:
- * Return: size_t
+ * @tree: parameter of node parent and child
+ * Return: if the tree is perfect or not
  */
 int binary_tree_is_perfect(const binary_tree_t *tree)
 {
+	if (tree == NULL)
+		return (0);
 
+	if (tree->left == NULL && tree->right == NULL)
+		return (0);
+
+	return (binary_tree_is_perfect(tree->left)
+		&& binary_tree_is_perfect(tree->right));
 }
